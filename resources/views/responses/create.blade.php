a30ce4c39b583e069c8150941c0bcda5cc5ad7bf
Raymond Nambaale <nambaaler@gmail.com>
2019-08-15 18:51:38 +0300

    submit survey response UI

--- resources/views/responses/create.blade.php
+++ resources/views/responses/create.blade.php
@@ -11,12 +11,38 @@
                 </div>
 
                 <div class="card-body">
-                    <form method="POST" action="/responses/{{ $survey->id }}">
+                    <form method="POST" action="/response/{{ $survey->id }}">
                         @csrf
 
                         @foreach ($survey->questions as $question)
+
+                            <input type="hidden" value="{{ $question->id }}" name="answers[{{ $question->id }}][question_id]"/>
+
                             <div class="form-group">
                                 <label>{{ $question->question_text }}</label>
+                                @switch($question->question_type)
+                                    @case('input')
+                                        <input type="" name="answers[{{ $question->id }}][answer_value]" class="form-control" {{ ($question->is_required) ? 'required' : }} >
+                                        @break
+                                    @case('radio')
+                                        @foreach ($question->choices as $choice)
+                                            <input type="radio" name="answers[{{ $question->id }}][answer_value]" class="form-control" {{ ($question->is_required) ? 'required' : }} > {{ $choice->choice_text }}
+                                        @endforeach
+
+                                    @case('checkbox')
+                                        @foreach ($question->choices as $choice)
+                                            <input type="checkbox" name="answers[{{ $question->id }}][answer_value]" class="form-control" {{ ($question->is_required) ? 'required' : }} > {{ $choice->choice_text }}
+                                        @endforeach
+                                        
+                                        @break
+
+                                    @case('textarea')
+                                        <textarea type="checkbox" name="answers[{{ $question->id }}][answer_value]" class="form-control" {{ ($question->is_required) ? 'required' : }} rows="4" ></textarea>
+                                        
+                                        @break
+                                    @default
+                                        
+                                @endswitch
                             </div>
                         @endforeach
                     </form>
