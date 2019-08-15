78272f3df262401be23045db3c573aea7790823e
Raymond Nambaale <nambaaler@gmail.com>
2019-08-15 18:58:39 +0300

    some DB seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Choice;
+use App\Question;
+use App\Survey;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +16,37 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
 
-        $base_choice = factory(Choice::class)->create();
+        $survey = factory(Survey::class)->create(['title' => 'This is a Sample Survey']);
+        $question = factory(Question::class)->create(
+            [
+                'survey_id'     => $survey->id,
+                'question_text' => 'What is your name',
+                'question_type' => 'input',
+                'is_required'   => TRUE
+            ]
+        );
+
+        $question2 = factory(Question::class)->create(
+            [
+                'survey_id' => $survey->id,
+                'question_text' => 'Are you human',
+                'question_type' => 'radio',
+                'is_required'   => TRUE
+            ]
+        );
+
+        $choice1 = factory(Choice::class)->create(
+            [
+                'choice_text' => 'Yes',
+                'question_id' => $question2->id
+            ]
+        );
+
+        $choice2 = factory(Choice::class)->create(
+            [
+                'choice_text' => 'No',
+                'question_id' => $question2->id
+            ]
+        );
     }
 }
